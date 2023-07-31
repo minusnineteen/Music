06a544ccc9190e13ef3e2e3d12b76b0760017a53
minusnineteen <117788629+minusnineteen@users.noreply.github.com>
2023-07-31 23:43:49 +0700

    Update PlayMusicActivity.java

--- app/src/main/java/com/example/music/PlayMusicActivity.java
+++ app/src/main/java/com/example/music/PlayMusicActivity.java
@@ -2,60 +2,166 @@ package com.example.music;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 
-public class PlayMusicActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity {
 
     Button prev, play, stop, next;
     TextView title, start, end;
     SeekBar sb;
+    ImageView img;
 
     ArrayList<Song> a;
-
+    MediaPlayer mp;
+    Animation anim;
     int pos = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_play_music);
+        setContentView(R.layout.activity_main);
 
+        prev = (Button) findViewById(R.id.btnPrev);
+        play = (Button) findViewById(R.id.btnPlay);
+        stop = (Button) findViewById(R.id.btnStop);
+        next = (Button) findViewById(R.id.btnNext);
+        title = (TextView) findViewById(R.id.tvSong);
+        start = (TextView) findViewById(R.id.tvStart);
+        end = (TextView) findViewById(R.id.tvEnd);
+        sb = (SeekBar) findViewById(R.id.seekBar);
+        img = (ImageView) findViewById(R.id.imageView);
 
+        addSong();
 
-            prev = (Button) findViewById(R.id.btnPrev);
-            play = (Button) findViewById(R.id.btnPlay);
-            stop = (Button) findViewById(R.id.btnStop);
-            next = (Button) findViewById(R.id.btnNext);
-            title = (TextView) findViewById(R.id.tv_SongName);
-            start = (TextView) findViewById(R.id.tvStart);
-            end = (TextView) findViewById(R.id.tvEnd);
-            sb = (SeekBar) findViewById(R.id.seekBar);
+        anim = AnimationUtils.loadAnimation(this, R.anim.rolate);
 
+        mp = MediaPlayer.create(MainActivity.this, a.get(pos).getFile());
+        title.setText(a.get(pos).getTitle());
 
+        play.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (mp.isPlaying()) {
+                    mp.pause();
+                    //play.setImageResource(R.drawable.);
+                } else {
+                    mp.start();
+                    //play.setImageResource(R.drawable.);
+                }
+                setTimeEnd();
+                setTimeStart();
+                img.startAnimation(anim);
+            }
+        });
 
-//            play.setOnClickListener(new View.OnClickListener() {
-//                @Override
-//                public void onClick(View view) {
-//                    MediaPlayer mp = MediaPlayer.create(MainActivity.this, a.get(pos).getFile());
-//                    mp.start();
-//                    title.setText(a.get(pos).getTitle());
-//                }
-//            });
+        prev.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                pos--;
+                if (pos < 0) {
+                    pos = a.size() - 1;
+                }
+                if (mp.isPlaying()) {
+                    mp.stop();
+                }
+                mp = MediaPlayer.create(MainActivity.this, a.get(pos).getFile());
+                title.setText(a.get(pos).getTitle());
+                mp.start();
+                setTimeEnd();
+                setTimeStart();
+            }
+        });
 
-//        Intent intent = getIntent();
-//        if (intent != null) {
-//            Song song = (Song) intent.getSerializableExtra("song");
-//            title.setText(song.getTitle());
-//        }
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                pos++;
+                if (pos > a.size() - 1) {
+                    pos = 0;
+                }
+                if (mp.isPlaying()) {
+                    mp.stop();
+                }
+                mp = MediaPlayer.create(MainActivity.this, a.get(pos).getFile());
+                title.setText(a.get(pos).getTitle());
+                mp.start();
+                setTimeEnd();
+                setTimeStart();
+            }
+        });
 
+        sb.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
 
-        }
+            }
 
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
 
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                mp.seekTo(sb.getProgress());
+            }
+        });
     }
+
+    private void addSong() {
+        a = new ArrayList<>();
+        a.add (new Song("Happy Birthday", R.raw.hbty));
+        a.add (new Song("Happy New Year", R.raw.hny));
+    }
+
+    private void setTimeEnd() {
+        SimpleDateFormat fm = new SimpleDateFormat("mm:ss");
+        end.setText(fm.format(mp.getDuration()));
+        sb.setMax(mp.getDuration());
+    }
+
+    private void setTimeStart() {
+        Handler hl = new Handler();
+        hl.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                SimpleDateFormat fm = new SimpleDateFormat("mm:ss");
+                start.setText(fm.format(mp.getCurrentPosition()));
+                sb.setProgress(mp.getCurrentPosition());
+
+                mp.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                    @Override
+                    public void onCompletion(MediaPlayer mediaPlayer) {
+                        pos++;
+                        if (pos > a.size() - 1) {
+                            pos = 0;
+                        }
+                        if (mp.isPlaying()) {
+                            mp.stop();
+                        }
+                        mp = MediaPlayer.create(MainActivity.this, a.get(pos).getFile());
+                        title.setText(a.get(pos).getTitle());
+                        mp.start();
+                        setTimeEnd();
+                        setTimeStart();
+                    }
+                });
+
+                hl.postDelayed(this, 500);
+            }
+        }, 100);
+    }
+
+}
